e8dfb23873bfd8c65f3b2544adbde7816cf5ddd8
Ronan-Leticia <ronanvictor100@gmail.com>
2023-04-18 20:30:41 -0300

    Foi criado a rota depositar

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const PORT = 3000;
 let saldo = 0;
 
 fastify.get('/saldo', (req, res) => {
-res.status(200).send(`Seu saldo é ${saldo}`);
+    res.status(200).send(`Seu saldo é ${saldo}`);
+});
 
+fastify.post('/depositar', (req, res) => {
+    let valor = Number(req.body.valor);
+    saldo = saldo + valor;
+    res.status(201).send(`Valor depositado com sucesso`);
 });
 
 fastify.listen(PORT, (err) => {
@@ -14,3 +19,4 @@ fastify.listen(PORT, (err) => {
     console.log(`Servidor rodando na porta ${PORT}`)
 });
 //restify
+
